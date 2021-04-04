d1f5164d7bd6fba99d5eeaf905dce2ec516cc719
Huang Tzu-Yen <akindofhush@hotmail.com>
2021-04-04 15:30:28 +0800

    parser Data

--- MusicSearchApp/Models/MusicData.swift
+++ MusicSearchApp/Models/MusicData.swift
@@ -14,16 +14,31 @@ struct MusicList : Decodable {
 }
 
 struct MusicData : Decodable {
-    let wrapperType: String
-    let explicitness: String
-    let kind: String
-    let trackName: String
-    let artistName: String
-    let collectionName: String
-    let censoredName: String
-    let artworkUrl60: String?
-    let artworkUrl100: String?
-    let viewURL: String
-    let previewUrl: String?
-    let trackTimeMillis: String?
+    let wrapperType, kind: String
+    let artistID, collectionID, trackID: Int
+    let artistName, collectionName, trackName, collectionCensoredName: String
+    let trackCensoredName: String
+    let artistViewURL, collectionViewURL, trackViewURL: String
+    let previewURL: String?
+    let artworkUrl30, artworkUrl60, artworkUrl100: String?
+    let collectionPrice, trackPrice: Double
+    let releaseDate: String
+    let collectionExplicitness, trackExplicitness: String
+    let discCount, discNumber, trackCount, trackNumber: Int
+    let trackTimeMillis: Int?
+    let country, currency, primaryGenreName: String
+    let isStreamable: Bool
+    
+    enum CodingKeys: String, CodingKey {
+        case wrapperType, kind
+        case artistID = "artistId"
+        case collectionID = "collectionId"
+        case trackID = "trackId"
+        case artistName, collectionName, trackName, collectionCensoredName, trackCensoredName
+        case artistViewURL = "artistViewUrl"
+        case collectionViewURL = "collectionViewUrl"
+        case trackViewURL = "trackViewUrl"
+        case previewURL = "previewUrl"
+        case artworkUrl30, artworkUrl60, artworkUrl100, collectionPrice, trackPrice, releaseDate, collectionExplicitness, trackExplicitness, discCount, discNumber, trackCount, trackNumber, trackTimeMillis, country, currency, primaryGenreName, isStreamable
+    }
 }
